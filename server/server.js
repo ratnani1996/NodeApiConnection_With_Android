d1028f53ba7f31851f313f3c225b6d44e2954afd
ratnani1996 <himanshuratnani1996@gmail.com>
2018-03-24 19:52:57 +0530

    added a different method to connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const app = express();
-//requiring bodyParser
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 //create mongooDb connection
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 var url = `mongodb://localhost:27017/android`
-const conn = mongoose.createConnection(url)
-conn.once('connected', ()=> console.log(`Connection to the database up and runnning`))
-conn.on('err', (err)=> console.log(err))
+mongoose.connect(url)
+mongoose.connection
+        .once('connected', ()=> console.log(`Connection to the database up and running`))
+        .on('err', (err)=> console.log(err))
+
+//requiring bodyParser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
-
+//acquire routes
+app.use(require('./controller/routes'))
 
 
 app.listen(3000, ()=> console.log(`Running on localhost 3000`))
